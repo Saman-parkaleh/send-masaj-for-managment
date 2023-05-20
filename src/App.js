c3419afe3fd52ab957bdc49bbab0f51888e1deaa
saman <ssaman.parkaleh76@gmail.com>
2023-05-20 16:09:25 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const onsubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_zuajjy9",
+        "template_qfi6oey",
+        e.target,
+        "NAszIguKv1UQOiBKI"
+      )
+      .then(
+        (result) => {
+          if (result.status === 200) {
+            toast.success(" massage send for sucses", {
+              position: toast.POSITION.TOP_CENTER,
+              closeOnClick: true,
+            });
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containor  d-flex justify-content-center ">
+      <div className=" box ">
+        <div className=" heder  pt-5">
+          <h4 className=" text-center justify-content-center">
+            send masaj for managment
+          </h4>
+        </div>
+        <form className="" onSubmit={onsubmit}>
+          <div className="form-group">
+            <input
+              className="form-control p-3"
+              placeholder="your Name "
+              type="text"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              className="form-control p-3 "
+              placeholder="your email "
+            />
+          </div>
+          <div>
+            <textarea
+              name="message"
+              className="form-control p-3"
+              placeholder="fecher "
+            />
+          </div>
+          <div>
+            <input
+              type="submit"
+              className="btn fw-buld form-control p-3 mt-5"
+              value="Send "
+            />
+          </div>
+        </form>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './Bootstraps/bootstrap.min.css'
+import './Bootstraps/bootstrap.rtl.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
